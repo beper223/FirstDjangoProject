bd6a13cf418208ed33d10b683ac0baa323b5483a
beper223 <zashalovskiy@gmail.com>
2025-08-31 16:07:14 +0200

    HW 16 perform_destroy

--- src/taskmanager/views/category.py
+++ src/taskmanager/views/category.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
@@ -11,6 +12,11 @@ class CategoryViewSet(ModelViewSet):
 
     queryset = Category.objects.filter(is_deleted=False)
     serializer_class = CategorySerializer
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.deleted_at = timezone.now()
+        instance.save(update_fields=['is_deleted', 'deleted_at'])
 
     @action(detail=False, methods=['get'], url_path='count-tasks')
     def count_tasks(self, request):
